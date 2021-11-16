7044a4e06c651f318feab10a8639287283a655bb
AvatarGuo <guoyiAvatar@gmail.com>
2021-11-16 09:34:11 +0800

    后处理，postbase add
    几个核心api
    MonoBehavior.OnRenderImage(src,dst)
    mat.setfloat("_xxx",xxx)
    Graphic.blit(src,dst,mat)

---
+++ Assets/Script/Chapter_12/BrightnessSaturationAndContrast.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessSaturationAndContrast : PostEffectsBase
+{
+
+    public Shader BrightSatConShader;
+    private Material brightSatMat;
+
+    [Range(0.0f,3.0f)]
+    public float brightness;
+
+    [Range(0.0f, 3.0f)]
+    public float saturation;
+
+    [Range(0.0f, 3.0f)]
+    public float contrast;
+
+    public Material material
+    {
+        get
+        {
+            brightSatMat = CheckShaderAndCreateMaterial(BrightSatConShader, brightSatMat);
+            return brightSatMat;
+        }
+    }
+
+    void OnRenderImage(RenderTexture src,RenderTexture dst)
+    {
+        if (material != null)
+        {
+            material.SetFloat("_brightness", brightness);
+            material.SetFloat("_saturation", saturation);
+            material.SetFloat("_contrast", contrast);
+
+            Graphics.Blit(src,dst,material);
+        }
+        else
+        {
+            Graphics.Blit(src,dst);
+        }
+
+    }
+
+}
---
+++ Assets/Script/Chapter_12/PostEffectsBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class PostEffectsBase : MonoBehaviour
+{
+    private bool enable = true;
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        checkSupport();
+    }
+
+
+    protected bool checkSupport()
+    {
+        
+        if (!SystemInfo.supportsImageEffects || !SystemInfo.supportsRenderTextures)
+        {
+            Debug.LogWarning("this platform not support image effects!");
+            return false;
+        }
+
+        return true;
+    }
+
+    protected void notSupport()
+    {
+        enable = false;
+    }
+
+    protected void CheckResources()
+    {
+        bool support = checkSupport();
+        if (!support)
+        {
+            notSupport();
+        }
+    }
+
+
+    protected Material CheckShaderAndCreateMaterial(Shader shader , Material material)
+    {
+        if (shader == null)
+        {
+            return null;
+        }
+
+        if (shader.isSupported && material && material.shader == shader)
+        {
+            return material;
+        }
+
+        if (!shader.isSupported)
+        {
+            return null;
+        }
+
+        material = new Material(shader);
+        // It will not be destroyed when a new Scene is loaded
+        material.hideFlags = HideFlags.DontSave;
+
+        if (material)
+        {
+            return material;
+        }
+
+        //返回material
+        return null;
+    }
+}
