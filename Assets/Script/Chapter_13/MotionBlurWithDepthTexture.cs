ba598f77b5fc7c13b60acb90d42efc47e860572b
guoyi <470659586@qq.com>
2021-11-28 22:49:25 +0800

    提交下运动模糊的效果  获得cameradepthTexture 1.获取当前世界坐标 2.拿到上一帧的转换矩阵。 3对比获取速度 4.当前贴图速度 周围采样求平均
    提交下运动模糊的效果
    
    获得cameradepthTexture
    1.获取当前世界坐标
    2.拿到上一帧的转换矩阵。
    3对比获取速度
    4.当前贴图速度 周围采样求平均

---
+++ Assets/Script/Chapter_13/MotionBlurWithDepthTexture.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class MotionBlurWithDepthTexture : PostEffectsBase
+{
+
+    private Material _material;
+    public Shader motionBlurDepthShader;
+    public Material material
+    {
+        get
+        {
+            if (_material == null)
+            {
+
+                _material = CheckShaderAndCreateMaterial( motionBlurDepthShader , _material );
+            }
+            return _material;
+
+        }
+    }
+
+    [UnityEngine.Range(0f , 1.0f)]
+    public float blurSize = 0.5f;
+
+
+    private Camera _camera;
+
+    public Camera camera
+    {
+        get
+        {
+            if (_camera == null)
+            {
+                _camera = this.gameObject.GetComponent<Camera>();
+                Assert.IsTrue(_camera != null, string.Format("[MotionBlurWithDepthTexture] this object : {0} not have camera ", this.gameObject.name));
+            }
+            return _camera;
+        }
+    }
+
+
+    /// <summary>
+    /// 上一帧世界转ndc空间矩阵
+    /// </summary>
+    private Matrix4x4 _preWorld2ProjectMatrix;
+
+    void OnRenderTexture(RenderTexture src ,RenderTexture dst)
+    {
+
+        //现在的前提是只有当前帧的位置信息（x,y 即 uv. z 即 SAMPLE_DEPTH_TEXTURE 在shader中采样 ）
+        if (material != null && camera != null)
+        {
+            //前后两帧设置值
+            material.SetFloat("_BlurSize",blurSize);
+            material.SetMatrix("_PreWorlldToProjectMatrix", _preWorld2ProjectMatrix);
+            
+            //unity这里面是左乘的 p*v*m * A 
+            Matrix4x4 _curWorld2ProjectMatrix = camera.projectionMatrix * camera.worldToCameraMatrix;
+
+            //shader中采样的 x , y 和 z 值，都是0-1范围内的，要转化成ndc，然后再转化到 (贴图只能保存0-1范围内的值，但是法线的范围是-1,1范围，所以采样法线贴图需要 [0,1]*2 -1,先归一化到0，1范围内 )
+            Matrix4x4 _curProject2WorldMatirx = _curWorld2ProjectMatrix.inverse;
+            material.SetMatrix("_CurProjectToWorldMatrix", _curProject2WorldMatirx);
+
+            _preWorld2ProjectMatrix = _curWorld2ProjectMatrix;
+            //世界坐标的点 转到ndc坐标中
+            //
+
+            //指定的index
+            Graphics.Blit(src,dst,material);
+        }
+        else
+        {
+            Graphics.Blit(src,dst);
+        }
+    }
+
+
+    void OnEnable()
+    {
+        camera.depthTextureMode |= DepthTextureMode.Depth;
+    }
+}
---
+++ Assets/Shader/Chapter_13/MotionBlurWithDepthTextureShader.shader
@@ -0,0 +1,107 @@
+﻿Shader "Unity Shader book/Chapter13/MotionBlurWithDepthTextureShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) 	= "white" {}
+		_BlurSize("BlurSize",float) = 0.5
+
+	}
+	SubShader
+	{
+		CGINCLUDE
+
+		#include "UnityCG.cginc"
+		#include "HLSLSupport.cginc"
+
+		sampler2D _MainTex; //获取图片纹素，即当前的x,y信息
+		float2 _MainTex_TexelSize;  //x，y 就能表示了,zw 存储的是啥
+
+		float _BlurSize;  //模糊程度
+		float4x4 _PreWorlldToProjectMatrix;
+		float4x4 _CurProjectToWorldMatrix;
+		
+		
+		sampler2D _CameraDepthTexture;  //获取深度图
+
+
+		struct v2f
+		{
+			float4 	pos			:	SV_POSITION;
+			half2  	uv			:	TEXCOORD0;
+			half2 	uv_depth	:	TEXCOORD1;
+		};
+
+
+		v2f vert(appdata_img v){
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+
+			//木有用scale ，tile 就不用TransTex
+			o.uv = v.texcoord;
+			o.uv_depth = v.texcoord;
+
+			#if UNITY_UV_STARTS_AT_TOP
+				o.uv_depth.y = 1.0 - o.uv.y;
+			#endif
+
+			return o;
+		};
+
+		
+		fixed frag(v2f i):SV_Target
+		{
+
+			//图片原始的x,y已经知道了，现在要获得深度值，但是深度值被压缩了，所以深度要变成线性深度值
+			float d = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture , i.uv.uv_depth);
+			//ndc空间本身z值就是非线性的
+			float4 projection_info =  float4(i.uv.x*2 - 1 ,u.uv.y *2 -1 , d * 2 - 1 , 1.0 ); //都已经被限制到-1，1了，所以直接w取1
+
+			float4 d 	= mul( _CurProjectToWorldMatrix, projection_info);
+			float4 worldPos = d/d.w; //获取世界坐标
+
+			//
+			float4 curPos = projection_info;
+			//上一步骤获得的世界坐标，获取上一个世界坐标
+			float4 prePos = mul(_PreWorlldToProjectMatrix,worldPos );
+			prePos = prePos/prePos.w;
+
+
+			//计算下速度
+			float velocity = curPos.xy - prePos.xy;
+
+			float2 uv = i.uv;
+			float4 color  = tex2D(_MainTex,uv);
+
+			uv += velocity * _BlurSize;
+			for(int it =0 ; it < 3; it++){
+				uv += velocity * _BlurSize;
+				float4 velColor =  tex2D(_MainTex,uv);
+				color += velColor;
+			}
+			color/=3;
+
+			return fixed4(color.rgb ,1.0);
+
+		}
+
+
+		ENDCG
+
+
+		Pass
+		{
+			ZTest Always ZWrite off Cull off
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			ENDCG
+
+		}
+
+
+	}
+
+	Fallback off
+}
